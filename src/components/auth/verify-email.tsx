662aaee2e4684f680b3f19fa3579d0417322294c
RPrasetyoB <rpbasuki.dev@gmail.com>
2025-01-15 15:11:51 +0700

    style: add spinner verify email

--- src/components/auth/verify-email.tsx
+++ src/components/auth/verify-email.tsx
@@ -91,8 +91,9 @@ const VerifyEmail: React.FC<VerifyEmailProps> = ({ lang, className }) => {
             {IsVerifying ? (
               <div className="relative flex w-full item-center justify-center gap-4">
                 <div
-                  className="inline-block h-5 w-5 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
-                  role="status"
+                  className="inline-block h-5 w-5 animate-spin rounded-full border-4 border-solid border-current
+                   border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite]
+                   dark:text-white"
                 ></div>
                 <span>Verifying...</span>
               </div>
