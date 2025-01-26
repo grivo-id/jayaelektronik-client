8b86c46474843487b29a5725edc0bfc006a15397
itsaFan <steffansim@gmail.com>
2025-01-26 07:19:03 +0700

    add conditional rendering for brand image

--- src/components/cards/brand-card.tsx
+++ src/components/cards/brand-card.tsx
@@ -47,9 +47,18 @@ const BrandCard: React.FC<BannerProps> = ({
           classNameInner
         )}
       >
-        <div className='aspect-square flex items-center h-40 w-40'>
-          <Image src={brand_image} alt={brand_name || 'brand-image'} width={240} height={240} className='object-cover'  />
-        </div>
+        {brand_image && (
+          <div className="aspect-square flex items-center h-40 w-40">
+            <Image
+              src={brand_image}
+              alt={brand_name || 'brand-image'}
+              width={240}
+              height={240}
+              className="object-cover"
+            />
+          </div>
+        )}
+
         {effectActive && (
           <div className="absolute top-0 block w-1/2 h-full transform -skew-x-12 ltr:-left-full rtl:-right-full z-5 bg-gradient-to-r from-transparent to-white opacity-30 group-hover:animate-shine" />
         )}
