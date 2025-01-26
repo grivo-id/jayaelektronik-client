c107eb2476037944dd8bbed95462fadf700c1186
RPrasetyoB <rpbasuki.dev@gmail.com>
2025-01-26 18:47:08 +0700

    feat: modify default language to "ina" indonesia

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,13 @@
 import { Rubik } from 'next/font/google';
 import { Metadata } from 'next';
-
 import './[lang]/globals.css';
 
 const rubik = Rubik({
-  weight: ['300','400', '500', '600', '700'],
+  weight: ['300', '400', '500', '600', '700'],
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-rubik',
 });
-
-
 
 export const metadata: Metadata = {
   title: 'Jaya Elektronik',
@@ -18,16 +15,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  lang = 'ina',
 }: {
   children: React.ReactNode;
+  lang?: string;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning={true}>
-      <body
-        // to prevent any warning that is caused by third party extensions like Grammarly
-        className={`${rubik.variable}`}
-        suppressHydrationWarning={true}
-      >
+    <html lang={lang} suppressHydrationWarning={true}>
+      <body className={`${rubik.variable}`} suppressHydrationWarning={true}>
         {children}
       </body>
     </html>
