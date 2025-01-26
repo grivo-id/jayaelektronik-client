c71159ea5eb1a38792476f3013a56830f9626825
itsaFan <steffansim@gmail.com>
2025-01-26 08:12:46 +0700

    add  image conditional error check

--- src/components/common/search-product.tsx
+++ src/components/common/search-product.tsx
@@ -42,14 +42,25 @@ const SearchProduct: React.FC<SearchProductProps> = ({ lang, item }) => {
       className="flex items-center justify-start w-full h-auto group"
     >
       <div className="relative flex w-20 rounded-md overflow-hidden flex-shrink-0 cursor-pointer me-4">
-        <Image
-          src={product_image1 ?? searchProductPlaceholder}
-          width={70}
-          height={70}
-          alt={product_name || 'Product Image'}
-          className="object-cover bg-fill-thumbnail"
-          style={{ width: 'auto' }}
-        />
+        {product_image1 ? (
+          <Image
+            src={product_image1 ?? searchProductPlaceholder}
+            width={70}
+            height={70}
+            alt={product_name || 'Product Image'}
+            className="object-cover bg-fill-thumbnail"
+            style={{ width: 'auto' }}
+          />
+        ) : (
+          <Image
+            src={searchProductPlaceholder}
+            width={70}
+            height={70}
+            alt={'Product Image'}
+            className="object-cover bg-fill-thumbnail"
+            style={{ width: 'auto' }}
+          />
+        )}
       </div>
 
       <div className="flex flex-col w-full overflow-hidden">
