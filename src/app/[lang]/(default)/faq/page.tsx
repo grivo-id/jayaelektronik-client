a5eabc63acb06575203a20d5063d42ae89ad56cb
itsaFan <steffansim@gmail.com>
2025-03-11 00:46:44 +0700

    update metadata and jsonld

--- src/app/[lang]/(default)/faq/page.tsx
+++ src/app/[lang]/(default)/faq/page.tsx
@@ -3,10 +3,84 @@ import PageHeroSection from '@components/ui/page-hero-section';
 import { Metadata } from 'next';
 import Accordion from '@components/ui/accordion';
 import { faq } from '@settings/faq-settings';
+import Script from 'next/script';
 
-export const metadata: Metadata = {
-  title: 'FAQ',
-};
+export async function generateMetadata({
+  params,
+}: {
+  params: { lang: string };
+}): Promise<Metadata> {
+  const currentLang = params.lang || 'ina';
+
+  const titles = {
+    ina: 'FAQ - Pertanyaan Umum',
+    en: 'FAQ - Frequently Asked Questions',
+  };
+
+  const descriptions = {
+    ina: 'Temukan jawaban atas pertanyaan umum tentang produk dan layanan Jaya Elektronik di Jayapura. Baca FAQ kami untuk informasi lebih lanjut.',
+    en: 'Find answers to common questions about Jaya Elektronik’s products and services in Jayapura. Read our FAQ for more details.',
+  };
+
+  const openGraphDescs = {
+    ina: 'Butuh informasi tentang toko dan produk kami? Cek FAQ Jaya Elektronik untuk jawaban atas pertanyaan umum.',
+    en: 'Need information about our store and products? Check out Jaya Elektronik’s FAQ for answers to common questions.',
+  };
+
+  return {
+    title: titles[currentLang as keyof typeof titles],
+    description: descriptions[currentLang as keyof typeof descriptions],
+    keywords: [
+      'jayaelektronik faq',
+      'buy electronic online',
+      'order electronic online',
+      'jaya',
+      'elektronik',
+      'papua',
+      'jayapura',
+      'percetakan',
+      'abepura',
+      'toko elektronik',
+      'electronic store',
+      'purchase electronic products',
+    ],
+    authors: [
+      {
+        name: 'Jaya Elektronik & Grivo.id',
+        url: 'https://www.jayaelektronik.com',
+      },
+    ],
+    robots: 'index, follow',
+    openGraph: {
+      type: 'website',
+      url: `https://www.jayaelektronik.com/${currentLang}/faq`,
+      title: titles[currentLang as keyof typeof titles],
+      description: openGraphDescs[currentLang as keyof typeof openGraphDescs],
+      siteName: 'Jaya Elektronik',
+      images: [
+        {
+          url: 'https://firebasestorage.googleapis.com/v0/b/personal-d9ef9.appspot.com/o/jaya%2Fjaya.jpeg?alt=media&token=a2f5414d-b25f-492b-a04a-382d167ffed6',
+          alt: 'Jaya Elektronik - Electronics Store in Jayapura',
+        },
+      ],
+    },
+    twitter: {
+      title: titles[currentLang as keyof typeof titles],
+      card: 'summary_large_image',
+      site: `https://www.jayaelektronik.com/${currentLang}/faq`,
+      creator: 'Jaya Elektronik & Grivo.id',
+      images:
+        'https://firebasestorage.googleapis.com/v0/b/personal-d9ef9.appspot.com/o/jaya%2Fjaya.jpeg?alt=media&token=a2f5414d-b25f-492b-a04a-382d167ffed6',
+    },
+
+    alternates: {
+      languages: {
+        en: `https://www.jayaelektronik.com/en/faq`,
+        id: `https://www.jayaelektronik.com/ina/faq`,
+      },
+    },
+  };
+}
 
 export default async function Page({
   params: { lang },
@@ -15,8 +89,13 @@ export default async function Page({
     lang: string;
   };
 }) {
+  const currentLang = lang ? lang : 'ina';
+  const jsonLdData = generateJsonLd(currentLang);
   return (
     <>
+      <Script id="faq-jsonld" type="application/ld+json">
+        {JSON.stringify(jsonLdData)}
+      </Script>
       <PageHeroSection
         heroTitle="text-page-faq"
         className="faq-banner-area"
@@ -37,3 +116,26 @@ export default async function Page({
     </>
   );
 }
+
+const generateJsonLd = (currentLang: string) => {
+  const descriptions = {
+    ina: 'Temukan jawaban atas pertanyaan umum tentang produk dan layanan Jaya Elektronik di Jayapura. Baca FAQ kami untuk informasi lebih lanjut.',
+    en: 'Find answers to common questions about Jaya Elektronik’s products and services in Jayapura. Read our FAQ for more details.',
+  };
+
+  const jsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'WebSite',
+    name: 'FAQ',
+    description: descriptions[currentLang as keyof typeof descriptions],
+    url: `https://jayaelektronik.com/${currentLang}/faq`,
+    logo: 'https://firebasestorage.googleapis.com/v0/b/personal-d9ef9.appspot.com/o/jaya%2Fjaya.jpeg?alt=media&token=a2f5414d-b25f-492b-a04a-382d167ffed6',
+    potentialAction: {
+      '@type': 'SearchAction',
+      target: `https://jayaelektronik.com/${currentLang}/search?category={search_term_string}`,
+      'query-input': 'required category=search_term_string',
+    },
+  };
+
+  return jsonLd;
+};
--- src/app/[lang]/(default)/signin/page.tsx
+++ src/app/[lang]/(default)/signin/page.tsx
@@ -1,10 +1,87 @@
 import LoginForm from '@components/auth/login-form';
 import Divider from '@components/ui/divider';
 import { Metadata } from 'next';
+import Script from 'next/script';
 
-export const metadata: Metadata = {
-  title: 'Sign In',
-};
+export async function generateMetadata({
+  params,
+}: {
+  params: { lang: string };
+}): Promise<Metadata> {
+  const currentLang = params.lang || 'ina';
+
+  const titles = {
+    ina: 'Masuk',
+    en: 'Sign In',
+  };
+
+  const descriptions = {
+    ina: 'Masuk ke akun Jaya Elektronik Anda untuk melanjutkan belanja dan mengakses penawaran eksklusif.',
+    en: 'Sign in to your Jaya Elektronik account to continue shopping and access exclusive deals.',
+  };
+
+  const openGraphDescs = {
+    ina: 'Login ke akun Jaya Elektronik untuk pengalaman belanja yang lebih cepat dan mudah.',
+    en: 'Log in to your Jaya Elektronik account for a faster and smoother shopping experience.',
+  };
+
+  return {
+    title: titles[currentLang as keyof typeof titles],
+    description: descriptions[currentLang as keyof typeof descriptions],
+    keywords: [
+      'sign in Jaya Elektronik',
+      'login electronic store',
+      'access online shopping account',
+      'Jaya Elektronik login',
+      'electronic store Jayapura',
+      'where to buy electronics in Papua',
+      'TVs',
+      'home appliances',
+      'jaya',
+      'elektronik',
+      'papua',
+      'jayapura',
+      'percetakan',
+      'abepura',
+      'user account login',
+    ],
+    authors: [
+      {
+        name: 'Jaya Elektronik & Grivo.id',
+        url: 'https://www.jayaelektronik.com',
+      },
+    ],
+    robots: 'index, follow',
+    openGraph: {
+      type: 'website',
+      url: `https://www.jayaelektronik.com/${currentLang}/signin`,
+      title: titles[currentLang as keyof typeof titles],
+      description: openGraphDescs[currentLang as keyof typeof openGraphDescs],
+      siteName: 'Jaya Elektronik',
+      images: [
+        {
+          url: 'https://firebasestorage.googleapis.com/v0/b/personal-d9ef9.appspot.com/o/jaya%2Fjaya.jpeg?alt=media&token=a2f5414d-b25f-492b-a04a-382d167ffed6',
+          alt: 'Jaya Elektronik - Electronics Store in Jayapura',
+        },
+      ],
+    },
+    twitter: {
+      title: titles[currentLang as keyof typeof titles],
+      card: 'summary_large_image',
+      site: `https://www.jayaelektronik.com/${currentLang}/signin`,
+      creator: 'Jaya Elektronik & Grivo.id',
+      images:
+        'https://firebasestorage.googleapis.com/v0/b/personal-d9ef9.appspot.com/o/jaya%2Fjaya.jpeg?alt=media&token=a2f5414d-b25f-492b-a04a-382d167ffed6',
+    },
+
+    alternates: {
+      languages: {
+        en: `https://www.jayaelektronik.com/en/signin`,
+        id: `https://www.jayaelektronik.com/ina/signin`,
+      },
+    },
+  };
+}
 
 export default async function Page({
   params: { lang },
@@ -13,8 +90,14 @@ export default async function Page({
     lang: string;
   };
 }) {
+  const currentLang = lang ? lang : 'ina';
+  const jsonLdData = generateJsonLd(currentLang);
+
   return (
     <>
+      <Script id="signin-jsonld" type="application/ld+json">
+        {JSON.stringify(jsonLdData)}
+      </Script>
       <Divider />
       <div className="flex items-center justify-center">
         <div className="px-4 py-12 sm:py-16 lg:py-20 md:px-6 lg:px-8 2xl:px-10">
@@ -29,3 +112,21 @@ export default async function Page({
     </>
   );
 }
+
+const generateJsonLd = (currentLang: string) => {
+  const descriptions = {
+    ina: 'Masuk ke akun Jaya Elektronik Anda untuk melanjutkan belanja dan mengakses penawaran eksklusif.',
+    en: 'Sign in to your Jaya Elektronik account to continue shopping and access exclusive deals.',
+  };
+
+  const jsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'WebSite',
+    name: 'Sign In',
+    description: descriptions[currentLang as keyof typeof descriptions],
+    url: `https://jayaelektronik.com/${currentLang}/signin`,
+    logo: 'https://firebasestorage.googleapis.com/v0/b/personal-d9ef9.appspot.com/o/jaya%2Fjaya.jpeg?alt=media&token=a2f5414d-b25f-492b-a04a-382d167ffed6',
+  };
+
+  return jsonLd;
+};
